09302aaefa65aadb9ae60645f9278870ad99f57a
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-09-17 10:44:36 +0500

    api

--- pandemic_backend/src/server.js
+++ pandemic_backend/src/server.js
@@ -8,10 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
